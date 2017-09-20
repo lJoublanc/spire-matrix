914cdec069b972374365859baade39ab7fc81360
Luciano Joublanc <luciano@joublanc.com>
2017-09-20 09:24:15 +0100

    Change vector to col-major

--- src/main/scala/Vector.scala
+++ src/main/scala/Vector.scala
@@ -3,10 +3,10 @@ package math
 import scala.reflect.ClassTag
 
 trait vector { 
-  type Vector[T,C] = DenseMatrix[T,1,C]
-  /** A `Vector` is just a type alias for a one-row matrix. */
+  type Vector[T,M] = DenseMatrix[T,M,1]
+  /** A `Vector` is a type alias for a one-column matrix. */
   object Vector {
-    def fromArray[T : ClassTag,C <: Int](array : Array[T])(c : C) : Vector[T,C] = DenseMatrix.fromArray(array)(1,c)
-    def apply[T : ClassTag, C<: Int](xs : T*)(c : C) : Vector[T,C] = DenseMatrix.fromArray(xs.toArray)(1,c)
+    def fromArray[T : ClassTag,M <: Int](array : Array[T])(m : M) : Vector[T,M] = DenseMatrix.fromArray(array)(m,1)
+    def apply[T : ClassTag, M <: Int](xs : T*)(m : M) : Vector[T,M] = DenseMatrix.fromArray(xs.toArray)(m,1)
   }
 }
