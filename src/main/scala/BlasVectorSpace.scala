299c0d069d1f194c95ff7fb57c27e717cb3ef47a
Luciano Joublanc <luciano@joublanc.com>
2017-09-21 12:31:02 +0100

    Remove functor from vector space implementation. Compiles now.

--- src/main/scala/BlasVectorSpace.scala
+++ src/main/scala/BlasVectorSpace.scala
@@ -20,41 +20,52 @@ trait blasVectorSpace {
     * @tparm N Number of columns.
     */
   trait BLASMatrixOps[T,M <: Int, N <: Int] {
-    /** The BLAS library implementation used to perform optimized calculations where possible */
+    /** The BLAS library implementation used to perform optimized calculations where possible  */
     def blas : BLAS
-    val blasCopy : (Int, Array[T], Int, Array[T], Int) => Unit
+    val xcopy : (Int, Array[T], Int, Array[T], Int) => Unit
   }
 
-  trait BLASMatrixApplicative[M <: Int, N <: Int, Mat[_,_ <: Int,_ <: Int] <: DenseMatrix[_,_ <: Int,_ <: Int]] extends Applicative[({ type m[x] = Mat[x,M,N] })#m ] {
-    /** Returns the canonical bases (unit square) scaled by `a`. */
-    def pure[A](a : A) : Mat[A,M,N] = ???
-    /** Applies the function `ff` independently to each matrix element. */
-    def ap[A,B](ff : Mat[A => B,M,N])(fa : Mat[A,M,N]) : F[B,M,N] = ???
-  }
+  /** A Type Class for Vector Spaces of `m` x `n` matrices, using BLAS for all operations.  */
+  abstract class VectorSpaceMatrixInstance[T : Field : ClassTag, M <: Int : ValueOf, N <: Int : ValueOf]
+      (implicit override val blas : BLAS) extends VectorSpace[DenseMatrix[T,M,N],T] with BLASMatrixOps[T,M,N] {
+    type Matrix = DenseMatrix[T,M,N]
+    lazy val size = valueOf[M]*valueOf[N]
 
-  abstract class VectorSpaceMatrixInstance[Mat[_,_ <: Int,_ <: Int] <: DenseMatrix[_,_ <: Int,_ <: Int], T : Field : ClassTag, M <: Int : ValueOf, N <: Int : ValueOf]
-      (implicit override val blas : BLAS, A : Applicative[({ type m[x] = Mat[x,M,N] })#m])
-      extends VectorSpace[Mat[T,M,N],T] with BLASMatrixOps[T,M,N] {
-    type Matrix = Mat[T,M,N]
-
-    /** Level 1 BLAS subroutine.
-      * Subclasses of this trait delegate BLAS calls to the implementing/specializing class, via 
-      * the `values` member, which materialize the result.
+    /** BLAS subroutines.
+      * Implementing classes need to define these to resolve to the correct data type.
       */
-    protected sealed trait L1BLAS extends DenseMatrix[T,M,N] { val x : Matrix }
+    val xscal : (Int, T, Array[T], Int) => Unit
+    val xaxpy : (Int, T, Array[T], Int, Array[T], Int) => Unit
+
+    /** Reify Level 1 BLAS subroutine.
+      * This trait is used to build up a description of the expression, to allow optimisation where possible.
+      */
+    protected sealed trait L1BLAS extends DenseMatrix[T,M,N] { self : L1BLAS =>
+      val x : Matrix 
+      lazy val values : Array[T] = self match {
+        case SCAL(alpha,x) => withCopyOf(x)(xscal(x.values.length, alpha, _, x.stride))
+        case AXPY(alpha,x,y) => withCopyOf(y)(xaxpy(y.values.length, alpha, x.values, x.stride, _, y.stride))
+      }
+    }
     protected case class SCAL(alpha : T, x : Matrix) extends L1BLAS
     protected case class AXPY(alpha : T, x : Matrix, y : Matrix) extends L1BLAS
 
+    /* Vector Space Implementation */
+
+    /** Returns a matrix with all elements negated. */
     def negate(x: Matrix): Matrix = SCAL(scalar.negate(scalar.one), x)
 
-    def zero: Matrix = A.pure(scalar.zero)
+    /** Returns an m x n matrix with elements set to zero.*/
+    lazy val zero: Matrix = DenseMatrix.fromArray[T,M,N](Array.fill(size)(scalar.zero))
 
+    /** Matrix addition */  
     def plus(x: Matrix,y: Matrix): Matrix = (x,y) match {
       case (SCAL(a,x),y) => AXPY(a, x, y)
       case (x,SCAL(a,y)) => AXPY(a, y, x)
       case _ => AXPY(scalar.one, x, y)
     }
 
+    /** Element-wise multiplication */
     def timesl(r: T,v: Matrix): Matrix = SCAL(r, v)
 
     implicit def scalar: Field[T] = evidence$1
@@ -63,9 +74,9 @@ trait blasVectorSpace {
       * Many BLAS subroutines accumulate the result to one of the input parameters. This convenience function
       * can be used to create a copy of that parameter and passing it to the function, so it is not over-written.
       */
-    protected def withCopyOf(x : Matrix)(f : Array[T] => Unit) : Array[T] = {
+    private def withCopyOf(x : Matrix)(f : Array[T] => Unit) : Array[T] = {
       val outBuff : Array[T] = Array ofDim x.values.length
-      blasCopy(x.values.length, x.values, x.stride, outBuff, x.stride)
+      xcopy(x.values.length, x.values, x.stride, outBuff, x.stride)
       f(outBuff)
       outBuff
     }
@@ -75,18 +86,12 @@ trait blasVectorSpace {
   import blas.DenseMatrix
   implicit def denseMatrixOfDoubleVectorSpace[M <: Int : ValueOf, N <: Int : ValueOf]
       (implicit blas : BLAS, A : Applicative[({ type m[x] = DenseMatrix[x,M,N] })#m]) :
-      VectorSpaceMatrixInstance[DenseMatrix,Double,M,N] = 
-    new VectorSpaceMatrixInstance[DenseMatrix,Double,M,N] {
+      VectorSpaceMatrixInstance[Double,M,N] = 
+    new VectorSpaceMatrixInstance[Double,M,N] {
     import blas._
 
-    protected case class SCAL(alpha : Double, x : Matrix) extends L1BLAS {
-      override lazy val values = withCopyOf(x)(dscal(x.values.length, alpha, _, x.stride))
-    }
-
-    protected case class AXPY(alpha : Double, x : Matrix, y : Matrix) extends L1BLAS {
-      override lazy val values = withCopyOf(y)(daxpy(y.values.length, alpha, x.values, x.stride, _, y.stride))
-    }
-
-    def blasCopy = dcopy
+    val xcopy = dcopy
+    val xscal = dscal
+    val xaxpy = daxpy
   }
 }
