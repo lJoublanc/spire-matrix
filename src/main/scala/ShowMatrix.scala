f740b15a8399f561292a26b10a8825dcb24381bd
Luciano Joublanc <luciano@joublanc.com>
2017-09-22 13:17:30 +0100

    Removed dependency on Cats (re-wrote Show)

--- src/main/scala/ShowMatrix.scala
+++ src/main/scala/ShowMatrix.scala
@@ -1,26 +1,42 @@
 package math 
 
-import cats.Show
-import cats.syntax.show._
-
-import blas.DenseMatrix
 import java.nio.charset.Charset
 import java.nio.charset.StandardCharsets.UTF_16
 
 trait showMatrix {
+  /** Modeled after cats.show. It should be possible to directly replace it. */
+  trait Show[-T] extends AnyRef { def show(t : T) : String }
 
-  implicit def showFiniteMatrix[T : Show, M <: Int : ValueOf, N <: Int : ValueOf](implicit charset : Charset = Charset.defaultCharset()) = new Show[DenseMatrix[T,M,N]] {
-    
-    def show(x : DenseMatrix[T,M,N]) : String = {
-      if (valueOf[M] == 0 || valueOf[N] == 0) "Null Matrix (m or n == 0)"
+  implicit class ShowOpsSyntax[-T](t : T)(implicit S: Show[T]) extends AnyRef { def show : String = S.show(t) }
+
+  /** Given a formatting string (without the `f` interpolator), generate a `Show` */
+  protected[showMatrix] def from[T](f: T => String) = new Show[T]{ override def show(t : T) : String = f(t) }
+
+  implicit val doubleShow : Show[Double] = from[Double]( d => f"$d%.2f")
+  implicit val realShow : Show[Float] = from[Float]( d => f"$d%.2f")
+
+  implicit def consoleShowFiniteMatrix[T : Show, M <: Int : ValueOf, N <: Int : ValueOf]
+      (implicit charset : Charset = Charset.defaultCharset()) =
+  new Show[FiniteMatrix[T,M,N]] {
+    val (maxWidth,maxHeight) = (80/6,80)
+    val comma = ", "
+
+    protected def rowToStr(s : Seq[String]) : String = s.mkString(comma)
+
+    def show(x : FiniteMatrix[T,M,N]) : String = {
+      val showT = implicitly[Show[T]]
+     
+      if (x.size == 0) "[ Null Matrix (m x n = 0) ]"
+      else if (x.cols > maxWidth || x.rows > maxHeight) s"[ Matrix (${x.rows} x ${x.cols} - too large to display) ]"
+      else if (x.rows == 1) "[" ++ rowToStr(Seq.tabulate(x.size)(i => showT.show(x(0,i)))) ++ "]"
       else {
-        val rows : Seq[Seq[String]] = Seq.tabulate(valueOf[M],valueOf[N])(x(_,_).show)
+        val rows : Seq[Seq[String]] = Seq.tabulate(valueOf[M],valueOf[N])((i,j) => showT.show(x(i,j)))
         val maxWidth = rows.map(_.length).max
-        lazy val blankRow = Seq.fill(maxWidth + rows.length - 1)(' ').toString //include commas
+        lazy val blankRow = Seq.fill(maxWidth + rows.length - 1)(' ').toString
         if (charset == UTF_16) 
-           s"┌ $blankRow ┐" +: rows.map("│" ++ _.mkString(", ") ++ "│" ) :+ s"└ $blankRow ┘"
+           s"┌ $blankRow ┐" +: rows.map("│" ++ rowToStr(_) ++ "│" ) :+ s"└ $blankRow ┘"
         else 
-           rows.map("|" ++ _.mkString(", ") ++ "|")
+           rows.map("|" ++ rowToStr(_) ++ "|")
       }.mkString("\n")
     }
   }
