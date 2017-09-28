d982bb83a2dce2153e42095cbb6192643808842f
Luciano Joublanc <luciano@joublanc.com>
2017-09-28 12:40:43 +0100

    Add soft equality comparison for doubles

---
+++ src/main/scala/ULPOrder.scala
@@ -0,0 +1,54 @@
+package math
+
+import spire.algebra.{Ring,Order,Signed}
+import scala.specialized
+
+object ULPOrderSingle {
+  /** Convenience functions for introspecting IEEE-754 numbers. */
+  implicit class IEEE754Float(val f : Float) {
+     def i : Int = java.lang.Float.floatToIntBits(f)
+     def isNegative : Boolean = i < 0
+     // def mantissa = i & ( (1 << 23) - 1 )
+     // def exponent = (i >> 23) & 0xFF
+  }
+}
+
+object ULPOrderDouble {
+  /** Convenience functions for manipulating IEEE-754 numbers. */
+  implicit class IEEE754Double(val f : Double) {
+     def i : Long = java.lang.Double.doubleToLongBits(f)
+     def isNegative : Boolean = i < 0
+  }
+}
+
+/**
+  * "Soft" equality for IEEE-754 double precision numbers using ULP.
+  * This will allow an approximate comparison of floating-point numbers.
+  * Mix this into an `xxxAlgebra` to override ordering.
+  * Overrides `eqv`, `neqv` etc. with the defaults form [[cats.kernel.Order]], that just forward to `compare`.
+  * Adapted from `randomascii`.
+  * @see <href a=https://randomascii.wordpress.com/2012/02/25/comparing-floating-point-numbers-2012-edition/>section "ULP, he said nervously"</href>
+  */
+trait ULPOrderDouble extends Order[Double] {
+  import ULPOrderDouble._
+  import java.lang.Math.abs
+  val δ : Long = 50
+
+  override def eqv(x: Double, y: Double): Boolean = compare(x, y) == 0
+  override def neqv(x: Double, y: Double): Boolean = compare(x, y) != 0
+  override def lteqv(x: Double, y: Double): Boolean = compare(x, y) <= 0
+  override def lt(x: Double, y: Double): Boolean = compare(x, y) < 0
+  override def gteqv(x: Double, y: Double): Boolean = compare(x, y) >= 0
+  override def gt(x: Double, y: Double): Boolean = compare(x, y) > 0
+  override def compare(x: Double, y: Double): Int =  {
+    if (x.isNegative != y.isNegative) {
+      if (x == y) 0  // +0 == -0
+      else if (x.isNegative) -1 else +1
+    }
+    else { 
+      val diff : Long = (x.i - y.i)
+      if ( abs(diff) <= δ ) 0
+      else if (diff < 0) -1 else +1
+    }
+}
+}
--- src/test/scala/blas/BlasVectorSpaceSpec.scala
+++ src/test/scala/blas/BlasVectorSpaceSpec.scala
@@ -1,16 +1,13 @@
 package blas
 
-import spire.std.double._
-import spire.algebra.VectorSpace
+import spire.algebra.{VectorSpace}
 import spire.laws._ // required for implicit discipline.Predicate
-import spire.laws.VectorSpaceLaws
 import blas._
 
 import org.typelevel.discipline.scalatest.Discipline
-import org.typelevel.discipline.Predicate
 import org.scalatest.FunSuite
 import org.scalacheck.{Arbitrary,Gen}
-import org.scalacheck.Arbitrary._
+import org.scalacheck.Arbitrary.arbitrary
 import org.scalacheck.util.Buildable
 
 import scala.reflect.ClassTag
@@ -18,21 +15,26 @@ import scala.reflect.ClassTag
 import com.github.fommil.netlib.{BLAS,F2jBLAS}
 
 class BlasVectorSpaceSpec extends FunSuite with Discipline {
-
+  import math.ULPOrderDouble
   import math.finiteMatrixEqInstance
 
+  // Override [in]equality operators
+
   implicit val blasInstance : BLAS = new F2jBLAS()
-  
+
+  // These two allow us to do 'soft' comparison of floating point numbers.
+  implicit object DoubleAlgebra extends spire.std.DoubleAlgebra with ULPOrderDouble
+  implicit val arbDouble : Arbitrary[Double] = Arbitrary { Gen.choose(-1e6,1e6) }
+    
   implicit def arbitraryDoubleMatrix[T : Arbitrary : ClassTag, M <: Int : ValueOf, N <: Int : ValueOf] : Arbitrary[DenseMatrix[T,M,N]] = {
     implicit val builder = implicitly[Buildable[T,Array[T]]]
-    Arbitrary { 
+    Arbitrary {
       Gen.containerOfN[Array,T](valueOf[M]*valueOf[N], arbitrary[T]) map { as =>
         blas.Matrix.fromDenseArray[T,M,N](as)
       }
     }
   }
          
-
   checkAll("BLAS double precision dense matrix (square)", VectorSpaceLaws[DenseMatrix[Double,2,2],Double].vectorSpace)
   checkAll("BLAS double precision dense matrix (rectangular)", VectorSpaceLaws[DenseMatrix[Double,2,3],Double].vectorSpace)
   checkAll("BLAS double precision dense matrix (column vector)", VectorSpaceLaws[DenseMatrix[Double,5,1],Double].vectorSpace)
